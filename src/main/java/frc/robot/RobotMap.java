78d9c185a160510bb63fe9249dd8327a81c6e867
SimonHelleman <hellemansimon@gmail.com>
2019-04-23 13:58:13 -0400

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,10 +6,10 @@ public interface RobotMap {
   final int CONTROLLER_DRIVER = 0;
 
   //Drivrtrain Motorcontroller CAN Id's
-  final int ID_LEFT_FRONT = 2;
-  final int ID_LEFT_BACK = 1;
-  final int ID_RIGHT_FRONT = 0;
-  final int ID_RIGHT_BACK = 3;
+  final int CAN_LEFT_FRONT = 2;
+  final int CAN_LEFT_BACK = 1;
+  final int CAN_RIGHT_FRONT = 0;
+  final int CAN_RIGHT_BACK = 3;
 
 
 }
